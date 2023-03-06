c11481946cbcb850b5a9c8950999e103f98d7481
Kelvin Oduro Aboagye <kelvinoduroaboagye12@gmail.com>
2023-03-06 12:58:46 +0000

    full code push

---
+++ src/components/meetups/NewMeetupForm.js
@@ -0,0 +1,62 @@
+import { useRef } from 'react' ;
+import Card from "../ui/Card";
+import classes from "./NewMeetupForm.module.css";
+function NewMeetupForm(props) {
+    const titleInputRef = useRef();
+    const imageInputRef = useRef();
+    const addressInputRef = useRef();
+    const descriptionInputRef = useRef();
+
+
+
+    function submitHandler(event){
+    event.preventDefault();
+    const enteredTitle = titleInputRef.current.value;    
+    const enteredImage = imageInputRef.current.value;
+    const enteredAdress = addressInputRef.current.value;
+    const enteredDescription  = descriptionInputRef.current.value; 
+
+    const meetupData = {
+        title : enteredTitle,
+        image : enteredImage,
+        address : enteredAdress,
+        description : enteredDescription
+    }
+props.onAddMeetup(meetupData)
+
+}
+  return (
+    <Card>
+      <form className={classes.form} onSubmit={submitHandler}>
+        <div className={classes.control}>
+        <label htmlFor="title">Meetup Title</label>
+        <input type="text" required id="title" ref ={titleInputRef}></input>
+        </div>
+
+        <div className={classes.control}>
+        <label htmlFor="title">Meetup Image</label>
+        <input type="url" required id="image" ref = {imageInputRef}></input>
+        </div>
+
+
+        <div className={classes.control}>
+        <label htmlFor="title">Address</label>
+        <input type="text" required id="address" ref = {addressInputRef}></input>
+        </div>
+
+        <div className={classes.control}>
+        <label htmlFor="title">Description</label>
+        <textarea row="5" required id="descriptioin" ref = {descriptionInputRef}></textarea>
+        </div>
+
+        <div className={classes.actions}>
+     
+        <button > Add Meetup</button>
+        </div>
+
+      </form>
+    </Card>
+  );
+}
+
+export default NewMeetupForm;
